af84d835e3e97dc8c83c5b8791063b07e8fe9019
FH <FH@MacBook-Air.local>
2016-10-25 09:38:56 -0500

    reviewed logic and made some changes - ready for formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var gamePlayersList ={
 	// id, class, image, attack power, health power, name, item referenece, 
-	playerData: [["player1", "players reset", "assets/images/player1.png", "5", "100", "snow chewbacca", 0], ["player2","players reset",  "assets/images/player2.png", "6", " 120", "hordi", 1], ["player3","players reset",  "assets/images/player3.png", "7", "105", "boba fett", 2], ["player4", "players reset", "assets/images/player4.png", "8", "150", "clone commander cody", 3]],
+	playerData: [["player1", "players reset", "assets/images/player1.png", "5", "100", "Snow Chewbacca", 0], ["player2","players reset",  "assets/images/player2.png", "6", " 120", "Hordi", 1], ["player3","players reset",  "assets/images/player3.png", "7", "105", "Boba Fett", 2], ["player4", "players reset", "assets/images/player4.png", "18", "150", "Clone Commander Cody", 3]],
 
 init: function(){
 
@@ -9,7 +9,6 @@ init: function(){
 		b = $('<div/>', {
 	        id: this.playerData[i][0],
 	        class: this.playerData[i][1],
-	       // src: this.playerData[i][2],
 	        value: this.playerData[i][3], 
 			name: this.playerData[i][5],
     	});
@@ -21,7 +20,6 @@ init: function(){
 		var c = "";
 		c = $('<img>', {src: this.playerData[i][2], 
 		    alt: this.playerData[i][5]});
-		// console.log(c);
 		var imageID = '#' + this.playerData[i][0];
 		$(imageID).append(c);
 
@@ -31,6 +29,8 @@ init: function(){
 
 var currentGame = {
 	gameStarted: null,
+	defenderName: "",
+	opponentName: "",
 	defender: null,
 	opponent: null,
 	defenderBaseHp: 0,
@@ -43,52 +43,33 @@ var currentGame = {
 	gameWinner: "",
 	attackCount: 0,
 	countOpponents: 0,
-
-	restart: function(){
-		// // reset currentgame object
-		// this.gameStarted = null;
-		// this.defender = null;
-		// this.opponent =null;
-		// this.defenderBaseHp =0;
-		// this.opponentBaseHp =0;
-		// this.defenderCurrentHp = 0;
-		// this.opponentCurrentHp = 0;
-		// this.defenderBaseAttack = 0;
-		// this.defenderCurrentAttack =0;
-		// this.opponentAttack = 0;
-		// this.gameWinner = "";
-		// this.attackCount = 0;
-		// this.countOpponents = 0;
-
-
-		// // empty messages -- or should do!
-		// $('.reset').empty();
-
-		// // set up again
-		// gamePlayersList.init();
-		// just reloads page?
-	   
-
-	
-	},
+	reloadGame: null,
 
 	gameInit: function(){
 		if(!this.gameStarted){
 			//set initial data
-			locaDefender = '#' + this.defender;
-			localOpponent  = '#' + this.opponent;
-			//console.log("this");
-			//console.log($('#defender').attr('value'));
-			this.defenderBaseHp = $(locaDefender).data('hp');
+			var localDefender = '#' + this.defender;
+			var localOpponent  = '#' + this.opponent;
+			// set base hp data
+			this.defenderBaseHp = $(localDefender).data('hp');
 			this.opponentBaseHp = $(localOpponent).data('hp');
-			this.defenderCurrentHp = this.defenderBaseHp;
+			// the below values don't change between games
 			this.opponentCurrentHp = this.opponentBaseHp;
-			this.defenderBaseAttack = $(locaDefender).attr('value');
-			this.defenderCurrentAttack = this.defenderBaseAttack;
+			this.defenderBaseAttack = $(localDefender).attr('value');
 			this.opponentAttack = $(localOpponent).attr('value');
-			this.attackCount = 0;
+			// the defender (player) Current HP and Attack Points are the base HP for the first opponent only
+			// they are carried over for the next games
+			console.log("count of oppoenents " + this.countOpponents);
+			if (this.countOpponents === 1) {
+				this.defenderCurrentHp = this.defenderBaseHp;
+				this.defenderCurrentAttack = this.defenderBaseAttack;
+			    this.attackCount = 0; // only reset to 0 for first opponent
+			}
+			// set initial counters and flags
 			this.gameStarted = true;
-			
+			//get current players names
+			this.defenderName =  $(localDefender).attr('name');
+			this.opponentName =  $(localOpponent).attr('name');
 		}
 
 	},
@@ -97,40 +78,47 @@ var currentGame = {
 	gamePlay: function(){
 		if ((this.gameStarted)) {
 		// check healthpoints of each player
-			if ((this.defenderCurrentHp <= 0) || (this.opponentCurrentHp) <= 0) {
+			if ((this.defenderCurrentHp <= 0) && (this.opponentCurrentHp <= 0)){
+				// if both go below 0 at same attack - both dead and no winner
+				$('#messages').html("You are both dead no one has won the game.<br>");		
+				$('#messages').append(' Select "Attack" or "Restart Game" to begin a new game. <br>');
+				// retart game
+				this.reloadGame = true;
+
+			}
+			else if (this.opponentCurrentHp <= 0) {
+				// figure out who won and send message
+				console.log('defender hp' + this.defenderCurrentHp);
+				console.log('opponent hp' + this.opponentCurrentHp);
+				// winning message
+				$('#messages').html(this.defenderName + " has won the game against " + this.opponentName  + " congratulations!<br>");		
 				// find another opponent and reset
 				this.opponent = null;
 				// reset game
 				this.gameStarted = null;
-				// empty oppenent
+				// remove opponent
 				$("#opponent").empty();
-				$('#defenderhp').empty();
+				// empty related messages
 	   		    $('#opponenthp').empty();
-					//message to select a new opponent
+				//message to select a new opponent
 				if (this.countOpponents < 3) {
-					$('#messages').html("Select a New Opponent to Start another game!");
+					$('#messages').append("Select a new Opponent to start another game!");
 				}
 				else
 				{
-					$('#messages').html("Game Over you have defeated all 3 Opponent!!");
+					$('#messages').append("Game Over you have defeated all 3 Opponent, Congratulations!!");
 				}
-
 			} 
-			// else if ((this.opponentCurrentHp) <= 0) {
-			// 		// find another opponent and reset
-			// 	this.opponent = null;
-			// 	// reset game
-			// 	this.gameStarted = null;
-			// 	// empty oppenent
-			// 	$("#opponent").empty();
-			// 	//message to select a new opponent
-			// 	$('message').html("Select a New Opponent to Start another game!");
-			// }
+			else if ((this.defenderCurrentHp) <= 0) {
+			    console.log('defender hp' + this.defenderCurrentHp);
+				console.log('opponent hp' + this.opponentCurrentHp);
+				$('#messages').html(this.opponentName + " has won the game against " + this.defenderName  + ", too bad sorry!<br>");
+				$('#messages').append(' Select "Attack" or "Restart Game" to begin a new game. <br>');
+				// reset the game
+				this.reloadGame = true;
+			}
 			else {
 				//play the game
-				// console.log("initialOp HP: " + this.opponentCurrentHp);
-				// console.log("initial Def HP: " + this.defenderCurrentHp);
-				// console.log("Attack" + this.defenderBaseAttack + " " + this.opponentAttack);
 				// reduce the HP of Defender by Opponent Base Attack Power
 				this.defenderCurrentHp = this.defenderCurrentHp - this.opponentAttack;
 				// reduce the HP of Opponent by Defender Current Attack Power
@@ -138,17 +126,12 @@ var currentGame = {
 				this.attackCount = this.attackCount + 1;
 				// Increase the Defender Attach Power by multiple of baseAttackPower
 				this.defenderCurrentAttack = this.defenderBaseAttack * (1 + this.attackCount);
-				// console.log("initial attach count" + this.attackCount);
-				
-				// console.log("Attack" + this.defenderCurrentAttack + " " + this.opponentAttack);
-				// console.log("Op HP: " + this.opponentCurrentHp);
-				// console.log("Def HP: " + this.defenderCurrentHp);
-				// console.log("new defender attack power " + this.defenderCurrentAttack);
-				// console.log("new attack count" + this.attackCount);
 
 				$('#messages').empty();
-	   		    $('#defenderhp').html("Defender Health Points" + currentGame.defenderCurrentHp);
-	   		    $('#opponenthp').html("Opponent Health Points" + currentGame.opponentCurrentHp);
+	   		    $('#defenderhp').html('Health Points:   ' + currentGame.defenderCurrentHp + '<br>');
+	   		    $('#defenderhp').append('Attack Power:   ' + currentGame.defenderCurrentAttack + '<br>');
+	   		    $('#opponenthp').html('Health Points:   ' + currentGame.opponentCurrentHp + '<br>');
+	   		    $('#opponenthp').append('Attack Power:    ' + currentGame.opponentAttack + '<br>');
 
 			}
 		}
@@ -160,36 +143,30 @@ $('document').ready(function(){
 	//loads the players into brower
 	gamePlayersList.init();
 
-	// user clicks a player to select it as defender and then opponent
-
+	// user clicks a player to select it as defender (player) and then opponent
 	$('.players').click(function(){
 	
 		if (!currentGame.defender){
 			console.log("going in here");
 			$('#defender').html(this); //moves to defender correct div area
-			 // var c = "";
-			 // c = $('<img/>',
-			 // 		{src: $(this).data('src')});
-			 // var d = '#' + $(this).attr("id");
-	
-			 // $(d).append(c);
-			 currentGame.defender = $(this).attr("id");
+			currentGame.defender = $(this).attr("id");
+			 // initial messages
+			$('#messages').empty();
+		    // health and attack data display
+	   		$('#defenderhp').html('Health Points:   ' + $(this).data("hp") + '<br>');
+	   		$('#defenderhp').append('Attack Power:   ' +  $(this).attr("value") +  '<br>');
+	   		
 			 
 		}
 		else if (!currentGame.opponent){
 			console.log("and in here");
 			 $('#opponent').html(this); //moves to opponent to correct div area
-			 var e = "";
-			 // e = $('<img/>',
-			 // 		{src: $(this).data('src')});
-			 // var f = '#' + $(this).attr("id");
-		
-			 // $(f).append(e);
 			 currentGame.opponent = $(this).attr("id");
-			 // console.log('currentGame.opponent' + currentGame.opponent);
 			 currentGame.countOpponents = currentGame.countOpponents + 1;
 			 console.log("count opponents " + currentGame.countOpponents);
-
+			 // health and attack data display
+			$('#opponenthp').html('Health Points:   ' + $(this).data("hp") + '<br>');
+	   		$('#opponenthp').append('Attack Power:    ' + $(this).attr("value") + '<br>');
 		}
 
 		 
@@ -198,13 +175,18 @@ $('document').ready(function(){
     // attack button logic
    $('#attack').click(function(){
 	    // make sure both players have been selected to begin playing
+	    if (currentGame.reloadGame){
+	    	// if true then restart the game
+	    	console.log("eloading game");
+	    	location.reload();
+	    	currentGame.reloadGame = null;
+	    }
 	   	if ((currentGame.defender !== null ) && (currentGame.opponent !== null)) {
 	   		currentGame.gameInit();
-	   		currentGame.gamePlay();
-	   		
+	   		currentGame.gamePlay();	
 	   	} 
 	   	else {
-	   		$('#messages').html("Select Players to begin game!");
+	   		$('#messages').html("Select both Player and Opponent to begin game!");
 	   	}
 
    });
